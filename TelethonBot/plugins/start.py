e58dda628b47cabd2487c47bb3a86b6a1d5808d9
DAMIEN <65272894+AmineSoukara@users.noreply.github.com>
2021-03-23 19:37:12 +0100

    Update start.py

--- TelethonBot/plugins/start.py
+++ TelethonBot/plugins/start.py
@@ -1,16 +1,16 @@
-# By < @xditya >
-# // @BotzHub //
+# @DamienSoukara
+
 from .. import BotzHub
 from telethon import events, Button
 
-@BotzHub.on(events.NewMessage(incoming=True, pattern="/start"))
+@Alty.on(events.NewMessage(incoming=True, pattern="/start"))
 async def start(event):
     await event.reply("Hello!",
                     buttons=[
-                        [Button.url("ButtonUrl", url="https://t.me/xditya")],
+                        [Button.url("ButtonUrl", url="https://t.me/DamienSoukara")],
                         [Button.inline("Inline Button",data="example")]
                     ])
 
-@BotzHub.on(events.callbackquery.CallbackQuery(data="example"))
+@Alty.on(events.callbackquery.CallbackQuery(data="example"))
 async def ex(event):
     await event.edit("You clicked a button!")
